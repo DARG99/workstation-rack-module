014dc3b88873df3ce6028eb302951ad4a4ecc7a5
Diogo Gomes <ctw04398@criticaltechworks.com>
2025-08-01 17:17:01 +0100

    tests

--- src/test/java/com/ctw/resource/RackResourceTest.java
+++ src/test/java/com/ctw/resource/RackResourceTest.java
@@ -16,38 +16,39 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
-//@QuarkusTest
-//public class RackResourceTest {
-//    @DisplayName("Should create a rack with a valid team")
-//    @Test
-//    public void testCreateRackWithTeam() {
-//
-//        TeamCreateDTO teamCreation = new TeamCreateDTO("QA Team", "string", "string"){};
-//
-//        String teamId =
-//                given().contentType(ContentType.JSON)
-//                        .body(teamCreation)
-//                        .when()
-//                        .post("/workstation/teams")
-//                        .then()
-//                        .statusCode(201)
-//                        .extract()
-//                        .path("id");
-//        System.out.println("Created team ID: " + teamId);
-//
-//        RackCreateDTO rackCreation = new RackCreateDTO("string", "AVAILABLE", "string", UUID.fromString(teamId));
-//
-//
-//        given()
-//                .body(rackCreation)
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .post("/workstation/racks")
-//                .then()
-//                .statusCode(201)
-//                .body("serialNumber", equalTo("string"))
-//                .body("status", equalTo("AVAILABLE"))
-//                .body("defaultLocation", equalTo("string"))
-//                .body("id", notNullValue());
-//    }
-//}
+@QuarkusTest
+public class RackResourceTest {
+    @DisplayName("Should create a rack with a valid team")
+    @Test
+    public void testCreateRackWithTeam() {
+
+        TeamCreateDTO teamCreation = new TeamCreateDTO("QA Team", "string", "string") {
+        };
+
+        String teamId =
+                given().contentType(ContentType.JSON)
+                        .body(teamCreation)
+                        .when()
+                        .post("/workstation/teams")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("id");
+        System.out.println("Created team ID: " + teamId);
+
+        RackCreateDTO rackCreation = new RackCreateDTO("string", "AVAILABLE", "string", UUID.fromString(teamId));
+
+
+        given()
+                .body(rackCreation)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/workstation/racks")
+                .then()
+                .statusCode(201)
+                .body("serialNumber", equalTo("string"))
+                .body("status", equalTo("AVAILABLE"))
+                .body("defaultLocation", equalTo("string"))
+                .body("id", notNullValue());
+    }
+}
